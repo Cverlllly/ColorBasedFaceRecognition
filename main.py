4f301538f04f350a7a0321ff769ce0c24569b6b9
Cverly <anej.cverle@gmail.com>
2025-04-13 18:30:19 +0200

    testing Workflows

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-VELIKOST_SKATLE = 50
+VELIKOST_SKATLE = 20
 
 
 def zmanjsaj_sliko(slika, sirina, visina):
@@ -20,27 +20,17 @@ def nared_skatle_na_sliki(slika, sirina_skatle, visina_skatle) -> list:
     for i in range(0, slika.shape[1], sirina_skatle):
         for j in range(0, slika.shape[0], visina_skatle):
             levo_zgoraj = (i, j)
-            desno_spodaj = (i + sirina_skatle, j + visina_skatle)
+            desno_spodaj = (min(i + sirina_skatle, slika.shape[1]), min(j + visina_skatle, slika.shape[0]))
             skatle.append((levo_zgoraj, desno_spodaj))
     return skatle
-
-
-def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
-    '''Sprehodi se skozi sliko v velikosti škatle (sirina_skatle x visina_skatle) in izračunaj število pikslov kože v vsaki škatli.
-    Škatle se ne smejo prekrivati!
-    Vrne seznam škatel, s številom pikslov kože.
-    Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
-      [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]].
-      V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
-    pass
 
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
     '''Prestej število pikslov z barvo kože v škatli.'''
     counter = 0
-    scaler=25
-    for i in range(slika.shape[1]):
-        for j in range(slika.shape[0]):
+    scaler = 40
+    for i in range(slika.shape[0]):
+        for j in range(slika.shape[1]):
             upper = barva_koze[0] + scaler, barva_koze[1] + scaler, barva_koze[2] + scaler
             lower = barva_koze[0] - scaler, barva_koze[1] - scaler, barva_koze[2] - scaler
 
@@ -66,44 +56,61 @@ def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) -> tuple:
             r += int(slika[j][i][2])
             counter += 1
 
-    # print(int(b / counter), int(g / counter), int(r / counter))
     return int(b / counter), int(g / counter), int(r / counter)
 
 
 def main():
     cam = cv.VideoCapture(0)
-    # prvi frame
     ret, frame1 = cam.read()
+    frame1 = zmanjsaj_sliko(frame1, 240, 320)
     # doloci sredino slike
     sredina = (frame1.shape[1] // 2, frame1.shape[0] // 2)
-    x_10 = frame1.shape[0] / 6
-    y_10 = frame1.shape[1] / 6
-    levo_zgoraj = (int(sredina[0] - x_10), int(sredina[1] - y_10))
-    desno_spodaj = (int(sredina[0] + x_10), int(sredina[1] + y_10))
+    x_10 = frame1.shape[0] / 7
+    y_10 = frame1.shape[1] / 5
+    levo_zgoraj_box = (int(sredina[0] - x_10), int(sredina[1] - y_10))
+    desno_spodaj_box = (int(sredina[0] + x_10), int(sredina[1] + y_10))
 
-    barva = doloci_barvo_koze(frame1, levo_zgoraj, desno_spodaj)
+    barva = doloci_barvo_koze(frame1, levo_zgoraj_box, desno_spodaj_box)
 
-    izris_kvadrata(frame1, levo_zgoraj, desno_spodaj)
-    frame1 = cv.flip(frame1, 1)
-    cv.imshow('frame1', frame1)
+    prev_tick = cv.getTickCount()
     while True:
         ret, frame = cam.read()
+        ret, frame1 = cam.read()
+        frame = zmanjsaj_sliko(frame, 240, 320)
+        frame1 = zmanjsaj_sliko(frame1, 240, 320)
         if not ret:
             print("Can't receive frame (stream end?)")
             break
         frame = cv.flip(frame, 1)
-        # obdelava slike
-        for i in range(0, frame.shape[1], VELIKOST_SKATLE):
-            for j in range(0, frame.shape[0], VELIKOST_SKATLE):
-                enbox = frame[j:j + VELIKOST_SKATLE, i:i + VELIKOST_SKATLE]
-                piksli = prestej_piklse_z_barvo_koze(enbox, barva)
-                if piksli > int(VELIKOST_SKATLE * VELIKOST_SKATLE / 2):
-                    cv.rectangle(frame, (i, j), (i + VELIKOST_SKATLE, j + VELIKOST_SKATLE), (0, 255, 0), 1)
-                else:
-                    cv.rectangle(frame, (i, j), (i + VELIKOST_SKATLE, j + VELIKOST_SKATLE), (0, 0, 255), 1)
+        frame1 = cv.flip(frame1, 1)
+
+        current_tick = cv.getTickCount()
+        time_diff = (current_tick - prev_tick) / cv.getTickFrequency()
+        fps = 1 / time_diff
+        prev_tick = current_tick
+
+        cv.putText(frame, f'FPS: {fps:.2f}', (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv.LINE_AA)
+
+        skatle = nared_skatle_na_sliki(frame, VELIKOST_SKATLE, VELIKOST_SKATLE)
+        for levo_zgoraj, desno_spodaj in skatle:
+            enbox = frame[levo_zgoraj[1]:desno_spodaj[1], levo_zgoraj[0]:desno_spodaj[0]]
+            piksli = prestej_piklse_z_barvo_koze(enbox, barva)
+            if piksli > int(VELIKOST_SKATLE * VELIKOST_SKATLE / 2):
+                cv.rectangle(frame, levo_zgoraj, desno_spodaj, (0, 255, 0), 1)
+            else:
+                cv.rectangle(frame, levo_zgoraj, desno_spodaj, (0, 0, 255), 1)
+        izris_kvadrata(frame1, levo_zgoraj_box, desno_spodaj_box)
         cv.imshow('frame', frame)
-        if cv.waitKey(1) == ord('q'):
+        cv.imshow('frame1', frame1)
+        key = cv.waitKey(1)
+        if key == ord('q'):
             break
+        elif key == ord('c'):
+            ret, frame1 = cam.read()
+            frame1 = zmanjsaj_sliko(frame1, 240, 320)
+            frame1 = cv.flip(frame1, 1)
+            barva = doloci_barvo_koze(frame1, levo_zgoraj_box, desno_spodaj_box)
+            izris_kvadrata(frame1, levo_zgoraj_box, desno_spodaj_box)
     cam.release()
     cv.destroyAllWindows()
 
